d249192af4585238ef61acbf3c7f9e0bf59dbfb1
Dustin <94391156+IronWill12@users.noreply.github.com>
2022-05-26 22:16:48 -0600

    mad libs

---
+++ pythonProject2/mad_libs_exercise.py
@@ -0,0 +1,20 @@
+# mad lib exercise
+
+entry_1 = input("Give me a type of animal (plural): ")
+entry_2 = input("Give me a location: ")
+entry_3 = input("Give me a number: ")
+entry_4 = input("Give me the name of a celebrity: ")
+entry_5 = input("Give me a color: ")
+
+
+output = f"""
+________________________________________
+Rise up my fellow {entry_1} of {entry_2}!
+All {entry_3} of you! Through our numbers we can make a difference.
+We must throw of the shackles and rebel against the vile {entry_4}.
+Be very afraid {entry_4}, the {entry_5} rebellion comes for you!!!
+________________________________________"""
+
+print(output)
+
+
