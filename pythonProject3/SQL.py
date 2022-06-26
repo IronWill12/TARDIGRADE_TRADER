13b79c5cd7b8ec49f959caa1f0d97f23f56ba474
Dustin <94391156+IronWill12@users.noreply.github.com>
2022-06-25 22:55:26 -0600

    things

--- pythonProject3/SQL.py
+++ pythonProject3/SQL.py
@@ -11,4 +11,19 @@ crsr = connection.cursor()
 print("Connected to the database")
 
 # close the connection
+connection.close(
+
+import sqlite3
+
+# connecting to the database
+connection = sqlite3.connect("gfg.db")
+
+# cursor
+crsr = connection.cursor()
+
+# print statement will execute if there
+# are no errors
+print("Connected to the database")
+
+# close the connection
 connection.close()
